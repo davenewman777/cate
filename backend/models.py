3ae4f0377488ead799576310380aecc108cb3bb2
davenewman777 <95712677+davenewman777@users.noreply.github.com>
2025-06-16 10:11:16 -0700

    remove junk

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-# models.py
-from pydantic import BaseModel
-from typing import List
-
-class SLARequest(BaseModel):
-    services: List[str]
-
-class SLAResponse(BaseModel):
-    composite_sla: float
